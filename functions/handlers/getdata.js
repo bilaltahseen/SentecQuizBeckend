9ab289d1404cbbe39712b6c30ae99e8acba70652
BuildTools <bilaltehseen@gmail.com>
2020-06-19 22:39:06 +0500

    Bug Fixed

--- functions/handlers/getdata.js
+++ functions/handlers/getdata.js
@@ -1,4 +1,4 @@
-const { db } = require('../util/admin');
+const { db, firestore } = require('../util/admin');
 
 exports.getdata = (req, res) => {
   let EasySET = [];
@@ -6,49 +6,116 @@ exports.getdata = (req, res) => {
   let HardSET = [];
   let random = req.randomArray;
   console.log(req.headers);
-  Promise.all([
-    db
-      .collection('QuestionCollection/Easy/QuestionsData')
-      .where('id', 'in', [...random])
-      .get()
-      .then((snapshot) => {
-        snapshot.forEach((document) => {
-          EasySET.push(document.data());
-        });
-      })
-      .catch((err) => {
-        console.error(err);
-        res.status(500).json(err);
-      }),
-    db
-      .collection('QuestionCollection/Medium/QuestionsData')
-      .where('id', 'in', [...random])
-      .get()
-      .then((snapshot) => {
-        snapshot.forEach((document) => {
-          MediumSET.push(document.data());
-        });
-      })
-      .catch((err) => {
-        console.error(err);
-        res.status(500).json(err);
-      }),
-    db
-      .collection('QuestionCollection/Hard/QuestionsData')
-      .where('id', 'in', [...random])
-      .get()
-      .then((snapshot) => {
-        snapshot.forEach((document) => {
-          HardSET.push(document.data());
-        });
-      })
-      .catch((err) => {
-        console.error(err);
-        res.status(500).json(err);
-      }),
-  ]).then(() => {
-    return res.status(200).json({
-      data: { EasySET: EasySET, MediumSET: MediumSET, HardSET: HardSET },
+
+  function shuffle(array) {
+    return array.sort(() => Math.random() - 0.5);
+  }
+
+  if (Math.round(Math.random())) {
+    Promise.all([
+      db
+        .collection('QuestionCollection/Easy/QuestionsData')
+        .orderBy('id', 'asc')
+        .limit(20)
+        .get()
+        .then((snapshot) => {
+          snapshot.forEach((document) => {
+            EasySET.push(document.data());
+          });
+        })
+        .catch((err) => {
+          console.error(err);
+          res.status(500).json(err);
+        }),
+      db
+        .collection('QuestionCollection/Medium/QuestionsData')
+        .orderBy('id', 'asc')
+        .limit(20)
+        .get()
+        .then((snapshot) => {
+          snapshot.forEach((document) => {
+            MediumSET.push(document.data());
+          });
+        })
+        .catch((err) => {
+          console.error(err);
+          res.status(500).json(err);
+        }),
+      db
+        .collection('QuestionCollection/Hard/QuestionsData')
+        .orderBy('id', 'asc')
+        .limit(20)
+        .get()
+        .then((snapshot) => {
+          snapshot.forEach((document) => {
+            HardSET.push(document.data());
+          });
+        })
+        .catch((err) => {
+          console.error(err);
+          res.status(500).json(err);
+        }),
+    ]).then(() => {
+      return res.status(200).json({
+        data: {
+          EasySET: shuffle(EasySET),
+          MediumSET: shuffle(MediumSET),
+          HardSET: shuffle(HardSET),
+        },
+      });
     });
-  });
+  } else {
+    Promise.all([
+      db
+        .collection('QuestionCollection/Easy/QuestionsData')
+        .orderBy('id', 'desc')
+        .limit(20)
+        .get()
+        .then((snapshot) => {
+          snapshot.forEach((document) => {
+            EasySET.push(document.data());
+          });
+        })
+        .catch((err) => {
+          console.error(err);
+          res.status(500).json(err);
+        }),
+      db
+        .collection('QuestionCollection/Medium/QuestionsData')
+        .orderBy('id', 'desc')
+        .limit(20)
+        .get()
+        .then((snapshot) => {
+          snapshot.forEach((document) => {
+            MediumSET.push(document.data());
+          });
+        })
+        .catch((err) => {
+          console.error(err);
+          res.status(500).json(err);
+        }),
+      db
+        .collection('QuestionCollection/Hard/QuestionsData')
+        .orderBy('id', 'desc')
+        .limit(20)
+        .get()
+        .then((snapshot) => {
+          snapshot.forEach((document) => {
+            HardSET.push(document.data());
+          });
+        })
+        .catch((err) => {
+          console.error(err);
+          res.status(500).json(err);
+        }),
+    ]).then(() => {
+      return res.status(200).json({
+        data: {
+          EasySET: shuffle(EasySET),
+          MediumSET: shuffle(MediumSET),
+          HardSET: shuffle(HardSET),
+        },
+      });
+    });
+  }
 };
--- functions/index.js
+++ functions/index.js
@@ -24,26 +24,13 @@ const { postscore } = require('./handlers/postscore');
 //ScoreGet
 const { getscore } = require('./handlers/getscore');
 
-//RandomGen
-const generateRandom = (req, res, next) => {
-  const max = 5;
-  let random = [];
-  for (let i = 0; i < max; i++) {
-    let temp = Math.floor(Math.random() * 6);
-    if (random.indexOf(temp) === -1 && temp !== 0) {
-      random.push(temp);
-    } else i--;
-  }
-  req.randomArray = random;
-  return next();
-};
 //getdataRoute
 app.use(helmet());
 app.use(cors);
 app.use(cookieParser);
 app.use(validateFirebaseIdToken);
 
-app.get('/getdata', generateRandom, getdata);
+app.get('/getdata', getdata);
 app.get('/getscore', getscore);
 //createdataRoute
 app.post('/postdata', createdata);
